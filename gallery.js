874bcaf3ea3549a5ee98cdcded0bae433b8ecb47
iTop <itop@192-168-0-100.rdsnet.ro>
2018-12-19 18:35:37 +0200

    image gallery html css js + images

---
+++ gallery.js
@@ -0,0 +1,14 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+  let newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
+
